aae87ba3e902de0e48c0f5faa02cdb22873c8aaa
NiceGamezwh <zwh20010228@gmail.com>
2026-01-14 09:23:43 +0800

    Fix typo in login text from '登陆' to '登录'

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function HomePage() {
             >
               <Link href="https://nobodysms.online/" target="_blank">
                 <Zap className="w-5 h-5 mr-2" />
-                立即登陆
+                立即登录
               </Link>
             </Button>
             <Button
@@ -188,7 +188,7 @@ export default function HomePage() {
               >
                 <Link href="https://nobodysms.online/" target="_blank">
                   <Zap className="w-6 h-6 mr-3" />
-                  立即登陆
+                  立即登录
                 </Link>
               </Button>
               <Button
